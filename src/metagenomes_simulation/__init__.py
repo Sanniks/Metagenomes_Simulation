e8ec6653894e65dcdfce53f1bf70eb62acfd735f
Carlo Mattia Lovecchio <carlo.mattia02@gmail.com>
2026-06-11 23:56:15 +0200

    [PY] Initialization of python library

---
+++ src/metagenomes_simulation/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from .__version__ import __version__
+
+__author__ = ["Carlo Mattia Lovecchio"]
+__email__ = ["carlo.lovecchio@studio.unibo.it"]
+
+__all__ = ["__version__"]
---
+++ src/metagenomes_simulation/__version__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = ["Carlo Mattia Lovecchio"]
+__email__ = ["carlo.lovecchio@studio.unibo.it"]
+
+__version__ = "0.0.1"
